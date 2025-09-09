b565a4e59d40a8459eb4d8600b788e2c3b080965
ilanalewitton2 <145459814+ilanalewitton2@users.noreply.github.com>
2025-09-09 15:27:54 +0000

    HW2

--- main.dart
+++ main.dart
@@ -1,1 +1,44 @@
+import 'dart:io';
+
+
+void main(){
+ List<int>numbers = [8, 9, 15, 24];
+ print("The numbrs are $numbers. What oppertation do you want to use to combine them?\n1. addition\n2. multiplication");
+  int option = int.parse(stdin.readLineSync()!);
+ int result = operation(option, numbers);
+
+
+ if (option == 1){
+   print("The sum of the numbers is $result.");
+ }
+ else {
+   print("The product of the numbers is $result.");
+ }
+
+}
+
+
+int operation(int choice, List<int> numbers){
+ 
+ switch(choice){
+   case 1:
+   int sum = 0;
+   for(var i=0; i<4; i++){
+     sum += numbers[i];
+   }
+   return sum;
+
+   case 2:
+   int product = 1;
+   for( var i=0; i<4; i++){
+     product *= numbers[i];
+   }
+   return product;
+
+   default:
+   print("No valid input. Returning 0.");
+   return 0;
+ }
+
+}
 
